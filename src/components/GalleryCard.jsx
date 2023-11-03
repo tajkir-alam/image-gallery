e2b139eb91235cb87378a4b109546ec71ad3a05d
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2023-11-03 23:43:39 +0600

    lazy loading added on img

--- src/components/GalleryCard.jsx
+++ src/components/GalleryCard.jsx
@@ -33,7 +33,7 @@ const GalleryCard = ({
       draggable
       className={`relative group rounded-lg overflow-hidden cursor-grab ${index === 0 && "lg:col-span-2 lg:row-span-2"} ${isDragging && index === draggingIndex ? "opacity-50" : ""}`}
     >
-      <img src={image.src} alt="" className={`border-2 rounded-lg w-full`} />
+      <img src={image.src} alt="Ollyo_Image_Gallery" className={`border-2 rounded-lg w-full`} loading="lazy" />
       <div
         className={`absolute top-0 w-full h-full bg-black/20 group-hover:opacity-100 duration-300 ${isSelected ? "opacity-20" : "opacity-0"}`}
       ></div>
